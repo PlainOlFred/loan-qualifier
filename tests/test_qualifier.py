0220e2914efa99a5b2d6c9016881fbde300f4986
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2023-05-21 19:24:56 -0500

    test for calculators function

---
+++ tests/test_qualifier.py
@@ -0,0 +1,8 @@
+# Import Calculators
+from utils import calculators
+
+def test_calculate_monthly_debt_ratio():
+  assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
+
+def test_calculate_loan_to_value_ratio():
+  assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
